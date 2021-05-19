6e8288fce699d6e9ca99849fc211bab2ac914b46
alla-andronova <allaitcreative@gmail.com>
2021-05-19 11:59:38 +0300

    deleted return in fetchCountries function

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,9 +1,7 @@
 const fetchCountries = searchQuery => {
-  return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`).then(
-    respons => {
-      return respons.json();
-    },
-  );
+  return fetch(
+    `https://restcountries.eu/rest/v2/name/${searchQuery}`,
+  ).then(respons => respons.json());
 };
 
 export default fetchCountries;
